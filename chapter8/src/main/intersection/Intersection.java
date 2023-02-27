4e9d3f3fecb1cfa28e58866d9f96a6bd663e3aeb
droidsam1 <droidsam@gmail.com>
2023-02-27 15:08:22 +0100

    add: return empty when no intersection

---
+++ chapter8/src/main/intersection/Intersection.java
@@ -0,0 +1,12 @@
+package intersection;
+
+public class Intersection {
+
+    private Intersection() {
+
+    }
+
+    public static int[] of(int[] arr1, int[] arr2) {
+        return new int[]{};
+    }
+}
--- chapter8/src/test/java/IntersectionTest.java
+++ chapter8/src/test/java/IntersectionTest.java
@@ -1,11 +1,19 @@
+import intersection.Intersection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class IntersectionTest {
 
+    private static final int[] EMPTY_ARRAY = new int[]{};
 
-    @Test void shouldJunitWorks() {
-        Assertions.assertTrue(true);
+
+    @Test void shouldReturnAnEmptyArrayWhenNotIntersection() {
+        var arr1 = new int[]{1};
+        var arr2 = new int[]{2};
+
+        var intersection = Intersection.of(arr1, arr2);
+
+        Assertions.assertArrayEquals(EMPTY_ARRAY, intersection);
     }
 
 }
