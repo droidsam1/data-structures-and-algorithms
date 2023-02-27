b7051f3a295e1e9419dd74ad4f3a66c1b80f9649
droidsam1 <droidsam@gmail.com>
2023-02-27 17:01:00 +0100

    refactor: reduce duplicity in tests

--- chapter8/src/test/java/IntersectionTest.java
+++ chapter8/src/test/java/IntersectionTest.java
@@ -1,7 +1,6 @@
 import intersection.Intersection;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,36 +9,24 @@ class IntersectionTest {
 
     private static final int[] EMPTY_ARRAY = new int[]{};
 
-    private static Stream<Arguments> shouldReturnOneElementOnlyWhenArraysHaveSameElementButNotInOrder() {
+    private static Stream<Arguments> shouldReturnIntersection() {
         return Stream.of(
                 //result, arr1, arr2
                 Arguments.of(new int[]{2}, new int[]{1, 2}, new int[]{2}),//
                 Arguments.of(new int[]{2}, new int[]{2}, new int[]{1, 2}),//
-                Arguments.of(new int[]{1, 2}, new int[]{1, 2}, new int[]{1, 2})//
+                Arguments.of(new int[]{1, 2}, new int[]{1, 2}, new int[]{1, 2}), //
+                Arguments.of(EMPTY_ARRAY, new int[]{2}, new int[]{1}),//
+                Arguments.of(new int[]{1}, new int[]{2, 1}, new int[]{1}), //
+                Arguments.of(
+                        new int[]{1, 3, 4, 10, 5, 9},
+                        new int[]{2, 1, 3, 4, 10, 5, 9},
+                        new int[]{9, 10, 3, 4, 5, 8, 12, 1}
+                )//
 
         );
     }
 
-
-    @Test void shouldReturnAnEmptyArrayWhenNotIntersection() {
-        var arr1 = new int[]{1};
-        var arr2 = new int[]{2};
-
-        var intersection = Intersection.of(arr1, arr2);
-
-        Assertions.assertArrayEquals(EMPTY_ARRAY, intersection);
-    }
-
-    @Test void shouldReturnOneElementOnlyWhenArraysHaveSameElement() {
-        var arr1 = new int[]{1};
-        var arr2 = new int[]{1};
-
-        var intersection = Intersection.of(arr1, arr2);
-
-        Assertions.assertArrayEquals(new int[]{1}, intersection);
-    }
-
-    @ParameterizedTest @MethodSource void shouldReturnOneElementOnlyWhenArraysHaveSameElementButNotInOrder(
+    @ParameterizedTest @MethodSource void shouldReturnIntersection(
             int[] expected, int[] arr1, int[] arr2
     ) {
         var intersection = Intersection.of(arr1, arr2);
