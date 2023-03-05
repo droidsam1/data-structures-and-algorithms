46c6d74ec761b2980fa93645030ebecbbb36c09b
droidsam1 <droidsam@gmail.com>
2023-03-05 11:07:47 +0100

    refactor: replace double loop with hashmap lookups

--- chapter8/src/main/java/find_first_non_duplicated_letter/FirstNonDuplicate.java
+++ chapter8/src/main/java/find_first_non_duplicated_letter/FirstNonDuplicate.java
@@ -1,25 +1,27 @@
 package find_first_non_duplicated_letter;
 
+import java.util.HashMap;
+
 public class FirstNonDuplicate {
 
     private FirstNonDuplicate() {
     }
 
     public static Character find(String input) {
+        var hashTable = new HashMap<Character, Integer>();
 
         var chars = input.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            var duplicated = false;
-            for (int j = 0; j < chars.length; j++) {
-                if (j != i && chars[i] == chars[j]) {
-                    duplicated = true;
-                    break;
-                }
-            }
-            if (!duplicated) {
-                return chars[i];
+        for (char aChar : chars) {
+            var counter = hashTable.getOrDefault(aChar, 0) + 1;
+            hashTable.put(aChar, counter);
+        }
+
+        for (char aChar : chars) {
+            if (hashTable.get(aChar) == 1) {
+                return aChar;
             }
         }
+
         return chars[0];
     }
 }
