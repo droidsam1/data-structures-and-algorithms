1d39e6e2b5daadd1246ece210fb1b845b2372ea2
droidsam1 <droidsam@gmail.com>
2023-03-07 21:49:39 +0100

    refactor: what I would use in real life instead

--- chapter9/src/main/java/reverse/ReverseString.java
+++ chapter9/src/main/java/reverse/ReverseString.java
@@ -1,24 +1,11 @@
 package reverse;
 
-import java.util.ArrayDeque;
-
 public class ReverseString {
 
     private ReverseString() {
     }
 
     public static String reverse(String input) {
-        var stack = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        var stringBuilder = new StringBuilder();
-        while (!stack.isEmpty()) {
-            stringBuilder.append(stack.pop());
-        }
-
-        return stringBuilder.toString();
-
+        return new StringBuilder(input).reverse().toString();
     }
 }
