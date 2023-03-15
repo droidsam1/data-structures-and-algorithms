6d4d916bce699ec475259d4b2f63da2281b8c400
droidsam1 <droidsam@gmail.com>
2023-03-15 22:51:44 +0100

    refactor: to parameterized test

--- chapter11/src/test/java/even_numbers/EvenNumbersTest.java
+++ chapter11/src/test/java/even_numbers/EvenNumbersTest.java
@@ -1,13 +1,19 @@
 package even_numbers;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class EvenNumbersTest {
 
-    @Test void shouldReturnEvenNumbersOnly() {
-        var input = new int[]{2};
-        var expected = new int[]{2};
+    private static Stream<Arguments> shouldReturnEvenNumbersOnly() {
+        return Stream.of(Arguments.of(new int[]{2}, new int[]{2}));
+    }
+
+
+    @ParameterizedTest @MethodSource void shouldReturnEvenNumbersOnly(int[] input, int[] expected) {
 
         var result = EvenNumbers.getEvenNumbers(input);
 
