e23192db52a31ff644f13d5ae21fcf7516f17f7b
droidsam1 <droidsam@gmail.com>
2023-03-15 23:08:43 +0100

    add: recursive solution

--- chapter11/src/main/java/even_numbers/EvenNumbers.java
+++ chapter11/src/main/java/even_numbers/EvenNumbers.java
@@ -1,11 +1,25 @@
 package even_numbers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvenNumbers {
 
     private EvenNumbers() {
     }
 
     public static int[] getEvenNumbers(int[] input) {
-        return input;
+        return getEvenNumbers(input, 0, new ArrayList<>()).stream().mapToInt(i -> i).toArray();
+    }
+
+    private static List<Integer> getEvenNumbers(int[] input, int index, List<Integer> result) {
+        if (index == input.length) {
+            return result;
+        }
+        if (input[index] % 2 == 0) {
+            result.add(input[index]);
+        }
+        return getEvenNumbers(input, ++index, result);
+
     }
 }
--- chapter11/src/test/java/even_numbers/EvenNumbersTest.java
+++ chapter11/src/test/java/even_numbers/EvenNumbersTest.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 class EvenNumbersTest {
 
     private static Stream<Arguments> shouldReturnEvenNumbersOnly() {
-        return Stream.of(Arguments.of(new int[]{2}, new int[]{2}));
+        return Stream.of(
+                Arguments.of(new int[]{2}, new int[]{2}),//
+                Arguments.of(new int[]{1, 2}, new int[]{2}),//
+                Arguments.of(new int[]{1, 2, 3, 4}, new int[]{2, 4}), //
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5, 6, 8, 10, 20, 22}, new int[]{0, 2, 4, 6, 8, 10, 20, 22})
+        );
     }
 
 
