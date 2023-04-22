5783c5708e47e70ad5a72d92fba87bd0a584584c
droidsam1 <droidsam@gmail.com>
2023-04-22 11:24:57 +0200

    refactor: change sort algorithm to merge sort

--- chapter13/src/main/java/exercise/one/GreatestProductOfAnyThree.java
+++ chapter13/src/main/java/exercise/one/GreatestProductOfAnyThree.java
@@ -18,22 +18,41 @@ public class GreatestProductOfAnyThree {
 
     private static int[] sort(int[] input) {
         var aCopy = input.clone();
-        sortInPlace(aCopy);
-        return aCopy;
+        return mergeSort(aCopy);
     }
 
-    private static void sortInPlace(int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            var indexOfMax = i;
-            for (int j = i; j < input.length; j++) {
-                if (input[j] > input[indexOfMax]) {
-                    indexOfMax = j;
-                }
+    private static int[] mergeSort(int[] input) {
+        //implement an insertion sort algorithm just to practice
+        if (input.length == 2) {
+            if (input[0] < input[1]) {
+                var temp = input[0];
+                input[0] = input[1];
+                input[1] = temp;
+                return input;
             }
-            //swap
-            var temp = input[i];
-            input[i] = input[indexOfMax];
-            input[indexOfMax] = temp;
+        } else if (input.length > 2) {
+            var middle = input.length / 2;
+            var left = java.util.Arrays.stream(input, 0, middle).toArray();
+            var right = java.util.Arrays.stream(input, middle, input.length).toArray();
+            return merge(mergeSort(left), mergeSort(right));
+
+
         }
+        return input;
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        var result = new int[left.length + right.length];
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (k < right.length && (j >= left.length || left[j] < right[k])) {
+                result[i] = right[k++];
+            } else if (j < left.length) {
+                result[i] = left[j++];
+            }
+
+        }
+        return result;
     }
 }
