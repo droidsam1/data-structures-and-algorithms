fa910bd5c8a6a82c262d95eeba598c3afa0a8846
droidsam1 <droidsam@gmail.com>
2023-04-23 12:32:38 +0200

    refactor: to repeated test annotation

--- sorting/src/test/java/org/example/SortingTest.java
+++ sorting/src/test/java/org/example/SortingTest.java
@@ -5,32 +5,40 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 class SortingTest {
 
-    public static Stream<Arguments> shouldSortInput() {
-        return Stream.of(
-                Arguments.of(new BubbleSort(), generateRandomArray()),
-                Arguments.of(new SelectionSort(), generateRandomArray()),
-                Arguments.of(new InsertionSort(), generateRandomArray())
-        );
-    }
+    private static final int REPETITIONS = 1000;
 
     private static int[] generateRandomArray() {
         return IntStream.generate(() -> new Random().nextInt(100) + 100).limit(1000).toArray();
     }
 
-    @ParameterizedTest
-    @MethodSource("shouldSortInput")
-    void shouldSortInput(SortingAlgorithm sortingAlgorithm, int[] input) {
-        int[] result = sortingAlgorithm.sort(input);
+    @RepeatedTest(REPETITIONS)
+    void shouldSortUsingBubbleSort() {
+        var input = generateRandomArray();
+        int[] result = new BubbleSort().sort(input);
 
         assertArrayEquals(Arrays.stream(input).sorted().toArray(), result);
     }
+
+
+    @RepeatedTest(REPETITIONS)
+    void shouldSortUsingSelectionSort() {
+        var input = generateRandomArray();
+        int[] result = new SelectionSort().sort(input);
+
+        assertArrayEquals(Arrays.stream(input).sorted().toArray(), result);
+    }
+
+    @RepeatedTest(REPETITIONS)
+    void shouldSortUsingInsertionSort() {
+        var input = generateRandomArray();
+        int[] result = new InsertionSort().sort(input);
+
+        assertArrayEquals(Arrays.stream(input).sorted().toArray(), result);
+    }
+
 
 }
