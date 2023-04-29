fc6d42672516f4c8584f96f5bf8d737f9fc992c7
droidsam1 <droidsam@gmail.com>
2023-04-29 17:59:34 +0200

    add: O(N2) implementation

--- chapter13/src/main/java/exercise/three/GreatestNumberInArray.java
+++ chapter13/src/main/java/exercise/three/GreatestNumberInArray.java
@@ -3,11 +3,10 @@ package exercise.three;
 public class GreatestNumberInArray {
 
     public static int in(int[] input) {
-        return sequentialSearch(input);
+        return exponentialSearch(input);
     }
 
     private static int sequentialSearch(int[] input) {
-
         int greatest = 0;
         for (int n : input) {
             if (n > greatest) {
@@ -15,6 +14,21 @@ public class GreatestNumberInArray {
             }
         }
         return greatest;
+    }
+    private static int exponentialSearch(int[] input) {
 
+        for (int i = 0; i < input.length; i++) {
+            var isTheGreatest = true;
+            for (int j = i; j < input.length; j++) {
+                if (input[i] < input[j]) {
+                    isTheGreatest = false;
+                    break;
+                }
+            }
+            if(isTheGreatest){
+                return input[i];
+            }
+        }
+        return input[0];
     }
 }
