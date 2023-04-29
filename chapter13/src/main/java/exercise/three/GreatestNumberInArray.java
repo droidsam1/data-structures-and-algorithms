439986ce688a0c9d45606a6562592b084f6dbc58
droidsam1 <droidsam@gmail.com>
2023-04-29 18:21:50 +0200

    add: logarithmic implementation

--- chapter13/src/main/java/exercise/three/GreatestNumberInArray.java
+++ chapter13/src/main/java/exercise/three/GreatestNumberInArray.java
@@ -3,7 +3,7 @@ package exercise.three;
 public class GreatestNumberInArray {
 
     public static int in(int[] input) {
-        return exponentialSearch(input);
+        return logarithmicSearch(input);
     }
 
     private static int sequentialSearch(int[] input) {
@@ -30,5 +30,59 @@ public class GreatestNumberInArray {
             }
         }
         return input[0];
+    }
+
+    private static int logarithmicSearch(int[] input) {
+        var sorted = sort(input);
+        return sorted[0];
+    }
+
+    private static int[] sort(int[] input) {
+        var aCopy = input.clone();
+        mergeSort(aCopy);
+        return aCopy;
+    }
+
+    private static void mergeSort(int[] aCopy) {
+        if (aCopy.length < 2) {
+            return;
+        }
+        var middle = aCopy.length / 2;
+        var left = new int[middle];
+        var right = new int[aCopy.length - middle];
+        for (int i = 0; i < aCopy.length; i++) {
+            if (i < middle) {
+                left[i] = aCopy[i];
+            } else {
+                right[i - middle] = aCopy[i];
+            }
+
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(aCopy, left, right);
+    }
+
+    private static void merge(int[] aCopy, int[] left, int[] right) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        for (int i = 0; i < aCopy.length; i++) {
+            if (leftIndex < left.length && rightIndex < right.length) {
+                if (left[leftIndex] > right[rightIndex]) {
+                    aCopy[i] = left[leftIndex++];
+                } else {
+                    aCopy[i] = right[rightIndex++];
+                }
+            } else {
+                if (leftIndex < left.length) {
+                    aCopy[i] = left[leftIndex++];
+                }
+                if (rightIndex < right.length) {
+                    aCopy[i] = right[rightIndex++];
+                }
+            }
+        }
+
     }
 }
