c9542d9fb24d26a1919273d164720fbc0a6bfc61
droidsam1 <droidsam@gmail.com>
2023-05-17 18:42:13 +0200

    refactor: switch to list internal implementation

--- chapter16/src/main/java/heap/BasicMaxHeap.java
+++ chapter16/src/main/java/heap/BasicMaxHeap.java
@@ -1,20 +1,46 @@
 package heap;
 
-public class BasicMaxHeap<T> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicMaxHeap<T extends Comparable<? super T>> {
 
 
-    private final Node<T> root;
+    private final List<T> data;
 
     public BasicMaxHeap(T[] input) {
-        this.root = new Node<>(input[0], null, null);
+        this.data = new ArrayList<>();
+        this.data.add(input[0]);
+        for (int i = 1; i < input.length; i++) {
+            this.data.add(input[i]);
+            if (input[i].compareTo(this.data.get(0)) > 0) {
+                swap(i);
+            }
+        }
+
     }
+
+    private void swap(int elementIndex) {
+        var currentElement = data.get(elementIndex);
+
+        var parentIndex = Math.max(elementIndex / 2 - 1, 0);
+
+        while (parentIndex >= 0) {
+            var parent = data.get(parentIndex);
+            if (currentElement.compareTo(parent) > 0) {
+                data.set(elementIndex, parent);
+                data.set(parentIndex, currentElement);
+                parentIndex = elementIndex / 2 - 1;
+            } else {
+                break;
+            }
+        }
+
+    }
+
 
     public T getTopElement() {
-        return root.value;
-    }
-
-    private record Node<T>(T value, Node<T> left, Node<T> right) {
-
+        return data.get(0);
     }
 }
 
--- chapter16/src/test/java/heap/BasicMaxHeapTest.java
+++ chapter16/src/test/java/heap/BasicMaxHeapTest.java
@@ -3,20 +3,29 @@ package heap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BasicMaxHeapTest {
 
+    private static Stream<Object> shouldReturnTheMax() {
+        return Stream.of(
+                Arguments.of((Object) new Integer[]{55}),
+                Arguments.of((Object) new Integer[]{55, 10}),
+                Arguments.of((Object) new Integer[]{10, 55})
 
-    @Test
-    void shouldReturnTheMax() {
-        var input = new Integer[]{55};
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource
+    void shouldReturnTheMax(Integer[] input) {
         var heap = new BasicMaxHeap<>(input);
 
         assertNotNull(heap);
         assertEquals(55, heap.getTopElement());
-
     }
 
 }
