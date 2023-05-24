b1763d62a3737467c44d0e703a97f9037a1c988a
droidsam1 <droidsam@gmail.com>
2023-05-24 17:43:13 +0200

    add: basic implementation of trie insertion

--- chapter17/src/main/java/trie/BasicTrie.java
+++ chapter17/src/main/java/trie/BasicTrie.java
@@ -12,9 +12,67 @@ public class BasicTrie {
         this.root = new Node(null, new ArrayList<>());
     }
 
+    public BasicTrie(Collection<String> words) {
+        this();
+        for (String word : words) {
+            var currentNode = this.root;
+            for (Character c : word.strip().toLowerCase().toCharArray()) {
+                if (currentNode.equals(root)) {
+                    if (currentNode.childrenNotContains(c)) {
+                        var newNode = new Node(c, new ArrayList<>());
+                        currentNode.children.add(newNode);
+                        currentNode = newNode;
+                        continue;
+                    }
+                }
+                var childWithCharacter = currentNode.getChildWith(c);
+                if (childWithCharacter != null) {
+                    currentNode = childWithCharacter;
+                } else {
+                    var newNode = new Node(c, new ArrayList<>());
+                    currentNode.children.add(newNode);
+                    currentNode = newNode;
+                }
+            }
+            if (currentNode != null) {
+                currentNode.setFinalWord();
+            }
+        }
+    }
 
-    private record Node(Character character, Collection<Node> children) {
+    public boolean contains(String word) {
+        var currentNode = this.root;
+        for (Character c : word.strip().toLowerCase().toCharArray()) {
+            var childWithCharacter = currentNode.getChildWith(c);
+            if (childWithCharacter != null) {
+                currentNode = childWithCharacter;
+            } else {
+                return false;
+            }
+        }
 
+        if (currentNode != null) {
+            return currentNode.isFinalWord;
+        }
+        return false;
+    }
+
+
+    private static class Node {
+
+        private final Character character;
+        private final Collection<Node> children;
+        private boolean isFinalWord;
+
+        Node(Character character, Collection<Node> children) {
+            this.character = character;
+            this.children = children;
+            this.isFinalWord = false;
+        }
+
+        public void setFinalWord() {
+            this.isFinalWord = true;
+        }
 
         Node getChildWith(Character c) {
             for (Node node : children) {
@@ -25,8 +83,8 @@ public class BasicTrie {
             return null;
         }
 
-        boolean childrenContains(Character c) {
-            return getChildWith(c) != null;
+        boolean childrenNotContains(Character c) {
+            return getChildWith(c) == null;
         }
     }
 
--- chapter17/src/test/java/trie/BasicTrieTest.java
+++ chapter17/src/test/java/trie/BasicTrieTest.java
@@ -1,18 +1,32 @@
 package trie;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class BasicTrieTest {
 
 
     @Test
-    void shouldCreateATrie() {
+    void shouldCreateATrieWithWords() {
 
-        var trie = new BasicTrie();
+        var trie = new BasicTrie(List.of("cat", "cap", "captain", "cut", "cop"));
 
         assertNotNull(trie);
 
+    }
+
+    @Test
+    void shouldContainWords() {
+
+        var inputWords = List.of("cat", "cap", "captain", "cop");
+
+        var trie = new BasicTrie(inputWords);
+
+        assertTrue(inputWords.stream().allMatch(trie::contains));
+        assertFalse(trie.contains("capo"));
     }
 }
